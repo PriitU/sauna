15b7027b6dad9142140f9362bb22f31720e160f8
PriitU <priit.ulmas@gmail.com>
2020-05-14 11:46:01 +0300

    Add sample dataset

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,10 @@
 </template>
 
 <script>
-import axios from 'axios';
+// import axios from 'axios';
 import Header from './components/layout/Header';
 import Graph from './components/Graph';
+import json from '../data.json';
 
 export default {
   name: 'App',
@@ -20,68 +21,73 @@ export default {
     return {
       options: {
         chart: {
-          id: 'sauna'
+          id: 'sauna',
+          zoom: {
+            autoScaleYaxis: true
+          }
         },
         xaxis: {
           type: 'datetime',
+          tickAmount: 6
+        },
+        dataLabels: {
+          enabled: false
+        },
+        legend: {
+          position: 'top',
+          horizontalAlign: 'left'
+        },
+        tooltip: {
+          x: {
+            format: 'dd MMM yyyy HH:mm:ss'
+          }
         }
       },
       series: [
         {
           name: 'Sauna',
-          data: [[1589278068000,30],
-                [1590278068000,40],
-                [1591278068000,45],
-                [1592278068000,50],
-                [1593278068000,49],
-                [1594278068000,60],
-                [1595278068000,70],
-                [1596278068000,91]]
+          data: []
         },{
           name: 'Air',
-          data: [[1589278068000,14],
-                [1590278068000,15],
-                [1591278068000,14],
-                [1592278068000,18],
-                [1593278068000,21],
-                [1594278068000,22],
-                [1595278068000,25],
-                [1596278068000,20]]
+          data: []
         },{
           name: 'Water',
-          data: [[1589278068000,10],
-                [1590278068000,12],
-                [1591278068000,15],
-                [1592278068000,14],
-                [1593278068000,19],
-                [1594278068000,20],
-                [1595278068000,18],
-                [1596278068000,19]]
+          data: []
         }
       ]
     }
   },
   created() {
-    axios.get(process.env.VUE_APP_ROOT_API + '/dev/saun-test')
-      .then(res => {
-        this.data = res.data;
-        const saunaData = res.data.map(s => {return {'x': s.timestamp, 'y': s.sauna}});
-        const airData = res.data.map(s => {return {'x': s.timestamp, 'y': s.air}});
-        const waterData = res.data.map(s => {return {'x': s.timestamp, 'y': s.water}});
-        const humidityData = res.data.map(s => {return {'x': s.timestamp, 'y': s.humidity}});
-        const uvData = res.data.map(s => {return {'x': s.timestamp, 'y': s.uv}});
-        const airPressureData = res.data.map(s => {return {'x': s.timestamp, 'y': s.airPressure}});
-        console.log(saunaData);
-        console.log(airData);
-        console.log(waterData);
-        console.log(humidityData);
-        console.log(uvData);
-        console.log(airPressureData);
-        //this.series = [{
-        //  data: res.data.sauna
-        //}]
-      })
-      .catch(err => console.log(err));
+    this.series = [{
+      data: json.map(s => {return {'x': s.timestamp, 'y': s.sauna}})
+    },{
+      data: json.map(s => {return {'x': s.timestamp, 'y': s.air}})
+    },{
+      data: json.map(s => {return {'x': s.timestamp, 'y': s.water}})
+    }]    
+    // axios.get(process.env.VUE_APP_ROOT_API)
+    //   .then(res => {
+    //     // const saunaData = res.data.map(s => {return {'x': s.timestamp, 'y': s.sauna}});
+    //     // const airData = res.data.map(s => {return {'x': s.timestamp, 'y': s.air}});
+    //     // const waterData = res.data.map(s => {return {'x': s.timestamp, 'y': s.water}});
+    //     //const humidityData = res.data.map(s => {return {'x': s.timestamp, 'y': s.humidity}});
+    //     //const uvData = res.data.map(s => {return {'x': s.timestamp, 'y': s.uv}});
+    //     //const airPressureData = res.data.map(s => {return {'x': s.timestamp, 'y': s.airPressure}});
+    //     //console.log(saunaData);
+    //     //console.log(airData);
+    //     //console.log(waterData);
+    //     //console.log(humidityData);
+    //     //console.log(uvData);
+    //     //console.log(airPressureData);
+    //     // this.series = [{
+    //     //   data: saunaData
+    //     // },{
+    //     //   data: airData
+    //     // },{
+    //     //   data: waterData
+    //     // }]
+    //   })
+    //   .catch(err => console.log(err));
   }  
 }
 </script>
