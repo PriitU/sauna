6aa1b8b57361a3de04903e4b7fccdb69b999b1ed
PriitU <priit.ulmas@gmail.com>
2020-05-24 22:38:06 +0300

    Add vuetify to project

--- src/App.vue
+++ src/App.vue
@@ -1,127 +1,60 @@
 <template>
-  <div id="app">
-    <Header />
-    <Stats  v-bind:series="series"/>
-    <Graph :options="options" :series="series" />
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-// import axios from 'axios';
-import Header from './components/layout/Header';
-import Graph from './components/Graph';
-import Stats from './components/Stats';
-import json from '../data.json';
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  components: {
-    Header,
-    Stats,
-    Graph
-  },
-  data() {
-    return {
-      options: {
-        chart: {
-          id: 'sauna',
-          zoom: {
-            autoScaleYaxis: true
-          }
-        },
-        stroke: {
-          show: true,
-          curve: 'smooth',
-          lineCap: 'butt',
-          colors: undefined,
-          width: 2,
-          dashArray: 0,
-        },
-        colors: ['#FCBA03','#00E396','#008FFB'],
-        xaxis: {
-          type: 'datetime',
-          tickAmount: 6
-        },
-        dataLabels: {
-          enabled: false
-        },
-        legend: {
-          position: 'top',
-          horizontalAlign: 'left'
-        },
-        tooltip: {
-          x: {
-            format: 'dd MMM yyyy HH:mm:ss'
-          }
-        }
-      },
-      series: [
-        {
-          name: 'Sauna',
-          data: []
-        },{
-          name: 'Air',
-          data: []
-        },{
-          name: 'Water',
-          data: []
-        }
-      ]
-    }
-  },
-  created() {
-    this.series = [{
-      name: 'Sauna',
-      data: json.map(s => {return {'x': s.timestamp, 'y': s.sauna}}),
-      latest : json[json.length-1].sauna,
-      latestUpdateTime : new Date(json[json.length-1].timestamp).toLocaleString('en-GB', { timeZone: 'UTC' })
-    },{
-      name: 'Air',
-      data: json.map(s => {return {'x': s.timestamp, 'y': s.air}}),
-      latest : json[json.length-1].air
-    },{
-      name: 'Water',
-      data: json.map(s => {return {'x': s.timestamp, 'y': s.water}}),
-      latest : json[json.length-1].water
-    }]
-    // Commented out due to static data being used (data.json)
-    // axios.get(process.env.VUE_APP_ROOT_API)
-    //   .then(res => {
-    //     const saunaData = res.data.map(s => {return {'x': s.timestamp, 'y': s.sauna}});
-    //     const airData = res.data.map(s => {return {'x': s.timestamp, 'y': s.air}});
-    //     const waterData = res.data.map(s => {return {'x': s.timestamp, 'y': s.water}});
-    //     //const humidityData = res.data.map(s => {return {'x': s.timestamp, 'y': s.humidity}});
-    //     //const uvData = res.data.map(s => {return {'x': s.timestamp, 'y': s.uv}});
-    //     //const airPressureData = res.data.map(s => {return {'x': s.timestamp, 'y': s.airPressure}});
-    //     //console.log(saunaData);
-    //     //console.log(airData);
-    //     //console.log(waterData);
-    //     //console.log(humidityData);
-    //     //console.log(uvData);
-    //     //console.log(airPressureData);
-    //     this.series = [{
-    //       data: saunaData,
-    //       latest : saunaData.slice(-1)[0].y,
-    //       latestUpdateTime : new Date(saunaData.slice(-1)[0].x).toLocaleString('en-GB', { timeZone: 'UTC' })
-    //     },{
-    //       data: airData,
-    //       latest : airData.slice(-1)[0].y,
-    //     },{
-    //       data: waterData,
-    //       latest : waterData.slice(-1)[0].y,
-    //     }]
-    //   })
-    //   .catch(err => console.log(err));
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
