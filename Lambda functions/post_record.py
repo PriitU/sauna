61d4dd5f12d0b759a57eaf37e151e1d232f24863
PriitU <priit.ulmas@gmail.com>
2020-05-30 09:47:31 +0300

    Add next update wait time to lambda response

--- Lambda functions/post_record.py
+++ Lambda functions/post_record.py
@@ -5,12 +5,33 @@ import time
 from datetime import datetime
 from dateutil import tz
 
+def tempAnalysis(air, sauna, hour):
+    '''Analysis of sauna state and custom response for the next update time
+        Inputs: air temperature
+                sauna temperature
+                current hour
+        Output: Time in minutes for the next update.
+    '''
+    tempDiff = sauna-air
+    if int(hour.hour) < 8:
+        responseTime = 15
+    elif tempDiff > 20:
+        # TODO: Create new lambda function to determine the direction of sauna temperature,
+        #       no need to log so often when the sauna is cooling down
+        responseTime = 2
+    else:
+        responseTime = 5
+    return responseTime
+
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb', region_name=os.environ['REGION'])
     table = dynamodb.Table(os.environ['TABLE'])
     TZ = tz.gettz('Europe/Tallinn')
     ts = time.time()
     dt_object = datetime.fromtimestamp(ts, TZ)
+
+    responseT = tempAnalysis(event['air'], event['sauna'], dt_object)
+    #print(dt_object.strftime('%-H'))
 
     event["timestamp"] = int(ts)
     event["date"] = dt_object.strftime('%Y-%m-%d %H:%M:%S')
@@ -24,6 +45,4 @@ def lambda_handler(event, context):
     event["humidity"] = int(event["humidity"]*100)
     table.put_item(Item=event)
 
-    return {"code":200, "message":"Item added to DB"}
-
-    #TODO: Analysis of sauna state and custom response of next update time
+    return {"code":200, "message":"Item added to DB", "responseTime":responseT}
